e133db5e740e9819ecfe0ab3388904306acaaf4a
lakshay2k <lakshaykmsharma@gmail.com>
2021-02-24 14:36:49 +0530

    Day10: Recursion using C++

---
+++ day10.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Complete the factorial function below.
+int factorial(int total, int num) {
+    if (num <= 1) {
+    return 1;
+} else {
+    return num*factorial(num, num - 1);
+}
+}
+
+int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int n;
+    cin >> n;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    int result = factorial(n,n);
+
+    fout << result << "\n";
+
+    fout.close();
+
+    return 0;
+}
